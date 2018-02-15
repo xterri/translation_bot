05874c9b76ed25278063f5729631d69707c6bcb8
xterri <thuynh1989@gmail.com>
2018-02-14 20:33:27 -0700

    testing server

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const verificationController = require('./controllers/verification');
-const webhookController = require('./controllers/webhook');
+//const verificationController = require('./controllers/verification');
+//const webhookController = require('./controllers/webhook');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set("port", process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 
 app.listen(app.get('port'), () => console.log("Server listening on port " + app.get('port')));
 
@@ -16,5 +16,5 @@ app.get('/', function(req, res) {
 //res.send("<html><head><title>Translation Bot</title></head><body><h1>Welcome</h1></body></html");
 });
 	
-app.get('/webhook', verificationController);
-app.post('/webhook', webhookController);
+//app.get('/webhook', verificationController);
+//app.post('/webhook', webhookController);
