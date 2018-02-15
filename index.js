b8f5b87b41190fa32bf9cde701331d2603b14908
xterri <thuynh1989@gmail.com>
2018-02-14 20:27:23 -0700

    updated index

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.set("port", process.env.PORT || 3000);
 app.listen(app.get('port'), () => console.log("Server listening on port " + app.get('port')));
 
 app.get('/', function(req, res) {
-	res.send("<html><head><title>Translation Bot</title></head><body><h1>Welcome</h1></body></html");
+	res.send("Hello I am a bot");	
+//res.send("<html><head><title>Translation Bot</title></head><body><h1>Welcome</h1></body></html");
 });
 	
 app.get('/webhook', verificationController);
