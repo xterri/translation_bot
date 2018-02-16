a3544dd54176dd10608af0597ce02bedc8a23ff5
xterri <thuynh1989@gmail.com>
2018-02-16 10:46:10 -0700

    added comments to processMessage & returning an empty string as default for practice

--- responses/practice.js
+++ responses/practice.js
@@ -1,5 +1,5 @@
-module.exports = (languageParam) => {
-    switch(languageParam) {
+module.exports = (result) => {
+    switch(result.parameters.Languages) {
         case "German":
             return "Okay, let's practice German!";
         case "English":
@@ -7,6 +7,10 @@ module.exports = (languageParam) => {
         case "Japanese": 
             return "Konnichiwa!";
         default:
+            // respond with "sorry" if some other language given
+            if (result.resolvedQuery === "French")
+                return "Sorry, we can only chat in one of the following options: German, English or Japanese";
+            }
             return "";
     }
 };
