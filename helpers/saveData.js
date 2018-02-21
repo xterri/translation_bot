f984dcf9ff0e65878bd7712a75eadd53b2bcbb45
xterri <thuynh1989@gmail.com>
2018-02-21 10:59:33 -0700

    back to the beginning

--- helpers/saveData.js
+++ helpers/saveData.js
@@ -1,25 +1,21 @@
-import fire from "../src/fire";
-// var firebase = require('firebase');
+var firebase = require('firebase');
 
-// let config = {
-//     apiKey: process.env.FIREBASE_API_KEY,
-//     authDomain: "tranbot-1.firebaseapp.com",
-//     databaseURL: "https://tranbot-1.firebaseio.com",
-//     projectId: "tranbot-1",
-//     storageBucket: "tranbot-1.appspot.com",
-//     messagingSenderId: process.env.FIREBASE_SENDER_ID
-// };
+var config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: "tranbot-1.firebaseapp.com",
+    databaseURL: "https://tranbot-1.firebaseio.com",
+    projectId: "tranbot-1",
+    storageBucket: "tranbot-1.appspot.com",
+    messagingSenderId: process.env.FIREBASE_SENDER_ID
+};
 
-// let app = firebase.initializeApp(config);
+var app = firebase.initializeApp(config);
 
 // get db
-    //let db = app.database();
+var db = app.database();
 
 // get reference to the 'users' node in db
-    //let usersRef = db.ref('users');
-
-// create reference to users in Firebase Database
-let usersRef = fire.database().ref('users').orderByKey();
+var usersRef = db.ref('users');
 
 module.exports = (userId, confirm, language) => {
 
