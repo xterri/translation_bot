0b2c0e2ae9d8184edacdaa359f644dc31f37122c
xterri <thuynh1989@gmail.com>
2018-02-21 12:01:06 -0700

    adding userId as category

--- helpers/saveData.js
+++ helpers/saveData.js
@@ -21,7 +21,7 @@ module.exports = (userId, confirm, language) => {
 
     console.log(">> in SaveData <<");
     console.log("id: " + userId + " language: " + language);
-    usersRef.push({userId});
+    usersRef.push(userId);
     let userRef = usersRef.ref(userId);
     userRef.push({
         id: userId,
