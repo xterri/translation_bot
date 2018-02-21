48ce91f38d3bae913c86fc9da1db7e9b201447ec
xterri <thuynh1989@gmail.com>
2018-02-21 10:53:00 -0700

    attempt to save data in database

--- helpers/saveData.js
+++ helpers/saveData.js
@@ -1,23 +1,28 @@
-var firebase = require('firebase');
+import fire from "../src/fire";
+// var firebase = require('firebase');
 
-let config = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: "tranbot-1.firebaseapp.com",
-    databaseURL: "https://tranbot-1.firebaseio.com",
-    projectId: "tranbot-1",
-    storageBucket: "tranbot-1.appspot.com",
-    messagingSenderId: process.env.FIREBASE_SENDER_ID
-};
+// let config = {
+//     apiKey: process.env.FIREBASE_API_KEY,
+//     authDomain: "tranbot-1.firebaseapp.com",
+//     databaseURL: "https://tranbot-1.firebaseio.com",
+//     projectId: "tranbot-1",
+//     storageBucket: "tranbot-1.appspot.com",
+//     messagingSenderId: process.env.FIREBASE_SENDER_ID
+// };
 
-let app = firebase.initializeApp(config);
+// let app = firebase.initializeApp(config);
 
 // get db
-let db = app.database();
+    //let db = app.database();
 
 // get reference to the 'users' node in db
-let usersRef = db.ref('users');
+    //let usersRef = db.ref('users');
+
+// create reference to users in Firebase Database
+let usersRef = fire.database().ref('users').orderByKey();
 
 module.exports = (userId, confirm, language) => {
+
     console.log(">> in SaveData <<");
     console.log("id: " + userId + " language: " + language);
     // let userRef = usersRef.ref(userId);
