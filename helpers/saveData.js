972bdda3875f72367f3fdf3c6f0d64c61bf3a01c
xterri <thuynh1989@gmail.com>
2018-02-23 15:02:45 -0700

    test

--- helpers/saveData.js
+++ helpers/saveData.js
@@ -71,7 +71,8 @@ module.exports = (cmd, userId, language) => {
         writeUserData(userId, language);
     } else if (cmd === "get") {
         // result should receive a "promise" to resolve to get the "language"
-        const result = getUserData(userId);
+        const result = getUserData(userId)
+            .then(data => data);
         console.log("result");
         console.log(result);
         return result;
